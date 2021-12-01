a55a06f66e393ac50cd591cc683b7dc0c9e04250
xWafl <73sampleperson@gmail.com>
2021-12-01 05:26:42 -0500

    feat day 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,12 +1,45 @@
 import * as path from "path";
 import { readInputSplitNum } from "../helpers/readInput";
 
+const part1 = (input: number[]) => {
+    let sum = 0;
+    for (let i = 1; i < input.length; i++) {
+        if (input[i] > input[i - 1]) {
+            sum++;
+        }
+    }
+    return sum;
+};
+
+const part2 = (input: number[]) => {
+    let sum = 0;
+    for (let i = 3; i < input.length; i++) {
+        if (
+            input[i] + input[i - 1] + input[i - 2] >
+            input[i - 1] + input[i - 2] + input[i - 3]
+        ) {
+            sum++;
+        }
+    }
+    return sum;
+};
+
 const main = async () => {
     const input = await readInputSplitNum(path.join(__dirname, "./input.txt"));
+    input.pop();
+    console.log(input);
 
-    console.time("main");
+    console.time("part1");
 
-    console.timeEnd("main");
+    console.log(part1(input));
+
+    console.timeEnd("part1");
+
+    console.time("part2");
+
+    console.log(part2(input));
+
+    console.timeEnd("part2");
 };
 
 main();
