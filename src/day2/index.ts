3c088e9b787218e3bae1015b779aa7cc42e76136
xWafl <73sampleperson@gmail.com>
2021-12-02 05:30:22 -0500

    feat: day 2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,12 +1,61 @@
 import * as path from "path";
-import { readInputSplitNum } from "../helpers/readInput";
+import { readInputSplit, readInputSplitNum } from "../helpers/readInput";
+
+const part1 = (input: string[][]) => {
+    let leftRight = 0;
+    let verti = 0;
+    for (const thing of input) {
+        if (thing[0] === "forward") {
+            leftRight += Number(thing[1]);
+        }
+        if (thing[0] === "down") {
+            verti += Number(thing[1]);
+        }
+        if (thing[0] === "up") {
+            verti -= Number(thing[1]);
+        }
+    }
+    console.log(leftRight, verti);
+    return leftRight * verti;
+};
+
+const part2 = (input: string[][]) => {
+    let leftRight = 0;
+    let verti = 0;
+    let aim = 0;
+    for (const thing of input) {
+        if (thing[0] === "forward") {
+            leftRight += Number(thing[1]);
+            verti += aim * Number(thing[1]);
+        }
+        if (thing[0] === "down") {
+            aim += Number(thing[1]);
+        }
+        if (thing[0] === "up") {
+            aim -= Number(thing[1]);
+        }
+    }
+    console.log(leftRight, verti);
+    return leftRight * verti;
+};
 
 const main = async () => {
-    const input = await readInputSplitNum(path.join(__dirname, "./input.txt"));
+    const input = await readInputSplit(path.join(__dirname, "./input.txt"));
+    input.pop();
+    const dirs = input.map(l => l.split(" "));
+    console.log(input);
 
-    console.time("main");
+    console.time("part1");
 
-    console.timeEnd("main");
+    console.log(part1(dirs));
+
+    console.timeEnd("part1");
+
+    console.time("part2");
+
+    console.log(part2(dirs));
+
+    console.timeEnd("part2");
 };
 
 main();
